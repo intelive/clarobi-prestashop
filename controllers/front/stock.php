9481592a44dc2190615f765f91c8b16802228421
Georgiana G <gitan_georgiana@yahoo.com>
2020-02-11 14:15:29 +0200

    go back to initial mapping for stocks

--- controllers/front/stock.php
+++ controllers/front/stock.php
@@ -29,32 +29,23 @@ class ClarobiStockModuleFrontController extends ClarobiApiModuleFrontController
         parent::initContent();
 
         try {
-
-            // todo come back to this mapping if the new one is incorrect
-//            $this->json = [
-//                'date' => date('Y-m-d', time()),
-//                'stock' => []
-//            ];
+            $this->json = [
+                'date' => date('Y-m-d', time()),
+                'stock' => []
+            ];
 
             foreach ($this->collection->stock_availables as $stock_available) {
                 if ($stock_available->id_product_attribute == 0) {
                     // Remove unnecessary keys
                     $simpleStockAvailable = $this->simpleMapping->getSimpleMapping('stock_available', $stock_available);
 
-                    // add date for each stock entry
-                    $simpleStockAvailable['entity_name'] = 'stock';
-                    $simpleStockAvailable['date'] = date('Y-m-d', time());
-
                     // Set to json
-//                    $this->json['stock'][] = $simpleStockAvailable;
-                    $this->json[] = $simpleStockAvailable;
+                    $this->json['stock'][] = $simpleStockAvailable;
                 }
             }
 
             // call encoder
             $this->encodeJson('stock');
-            /** @var StockAvailable $stock_available */
-            $this->encodedJson['lastId'] = ($stock_available ? $stock_available->id : 0);
 
             die(json_encode($this->encodedJson));
 
