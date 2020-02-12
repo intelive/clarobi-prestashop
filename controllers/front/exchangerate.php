a3e8ae66428d87b65c11c567cbe70b3f49969d9f
Georgiana G <gitan_georgiana@yahoo.com>
2020-02-12 10:21:45 +0200

    exchange rate endpoint

---
+++ controllers/front/exchangerate.php
@@ -0,0 +1,60 @@
+<?php
+
+include(_PS_MODULE_DIR_ . 'clarobi/controllers/front/api/api.php');
+
+class ClarobiExchangerateModuleFrontController extends ClarobiApiModuleFrontController
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->url = $this->shopDomain . '/api/currencies';
+    }
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function initContent()
+    {
+        try {
+            $this->json = [
+                'date' => date('Y-m-d H:i:s', time()),
+                'data' => []
+            ];
+
+            // todo other way of setting base currency is to provide input in configuration form
+            $base_currency = Currency::getDefaultCurrency();
+
+            $this->collection = $this->getCollection();
+
+            /** @var Currency $currency */
+            foreach ($this->collection->currencies as $currency) {
+                // Set to json
+                $this->json['data'][] = [
+                    'id' =>$currency->id,
+                    'from_currency' => $currency->iso_code,
+                    'base_currency' => ($base_currency ? $base_currency->iso_code : null),
+                    'rate' => $currency->conversion_rate,
+                    'created_at' => null,   // no date found
+                    'updated_at' => null
+                ];
+            }
+
+            // todo stock and exchange rate will be compressed and encoded ?
+
+            // call encoder
+            $this->encodeJson('exchangerate');
+
+            die(json_encode($this->encodedJson));
+
+        } catch (Exception $exception) {
+            $this->json = [
+                'status' => 'error',
+                'error' => $exception->getMessage()
+            ];
+            die(json_encode($this->json));
+        }
+    }
+
+}
